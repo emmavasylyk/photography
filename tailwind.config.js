e784a44b118c8b86852001f8860d5e8c5f951f97
emmavasylyk <emma.ovsiannikova91@gmail.com>
2024-07-08 15:26:37 +0300

    feat: add newsletter section

--- tailwind.config.js
+++ tailwind.config.js
@@ -34,6 +34,7 @@ module.exports = {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
         },
+        tertiary: { DEFAULT: "hsl(var(--tertiary))" },
         destructive: {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
